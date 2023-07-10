1eff103dec98e5fc6eb9c35784e09a3dfb922e34
Paul Hebert <paul@cloudfour.com>
2023-07-10 11:36:28 -0700

    New spiral helper

---
+++ bits/shapes/spiral-points.js
@@ -0,0 +1,24 @@
+import { randomDegree } from "randomness-helpers";
+import { angledPositionFromPoint } from "../../helpers/angled-position-from-point.js";
+
+export function spiralPoints({ x, y, r, angle = randomDegree() }) {
+  const points = [];
+
+  const delta = r / 75;
+
+  let distance = 0;
+
+  while (distance < r) {
+    const newPoint = angledPositionFromPoint({
+      angle,
+      distance,
+      point: { x, y },
+    });
+    points.push(newPoint);
+
+    distance += delta;
+    angle += 30;
+  }
+
+  return points;
+}
