de7df9c3c33f3771281fbeb498f406a842a5302c
Paul Hebert <paul@cloudfour.com>
2025-01-01 14:58:10 -0800

    WIP

--- art/genuary-2025-1/genuary-2025-1.js
+++ art/genuary-2025-1/genuary-2025-1.js
@@ -1,30 +1,45 @@
 import { buildSvg } from "../../helpers/build-svg.js";
 import { buildFunctionEndpoint } from "../../helpers/build-function-endpoint.js";
-import { randomHsl, randomHsla } from "randomness-helpers";
-import { randomInt } from "crypto";
+import {
+  randomHsl,
+  randomHsla,
+  random,
+  randomInt,
+  randomItemInArray,
+} from "randomness-helpers";
+import FastNoise from "fastnoise-lite";
 
-export const handler = buildFunctionEndpoint(() => {
+export const handler = buildFunctionEndpoint((seed) => {
+  const noise = new FastNoise();
+  const noiseType = randomItemInArray([
+    // "OpenSimplex2",
+    // "OpenSimplex2S",
+    // "Cellular",
+    "Perlin",
+    // "ValueCubic",
+    // "Value",
+  ]);
+  noise.SetNoiseType(FastNoise.NoiseType[noiseType]);
+  noise.SetSeed(seed);
+  noise.SetFrequency(random(0.05, 0.1));
+
   const squigglyLines = [];
 
   const height = 900;
   const width = 1600;
 
   const lineSpacing = [100, 200];
-  const xSpacing = 25;
+  const xSpacing = randomItemInArray([10, 20, 30, 40, 50]);
   const scaledWidth = width / xSpacing;
 
   let y = randomInt(...lineSpacing);
 
   while (y < height * 1.5) {
-    let localY = y;
-
     const newLine = [];
 
     for (let x = 0; x < scaledWidth; x++) {
-      console.log(x);
-      localY += randomInt(-20, 20);
-
-      newLine.push(localY);
+      const noiseVal = noise.GetNoise(x, y);
+      newLine.push(y + noiseVal * 100);
     }
 
     squigglyLines.push(newLine);
@@ -33,13 +48,13 @@ export const handler = buildFunctionEndpoint(() => {
 
   function drawVerticalLines(lines) {
     const svgLines = [];
-    const lineWidth = 25;
+    const lineWidth = xSpacing;
 
     for (let lineCount = 0; lineCount < lines.length; lineCount++) {
       for (let x = 0; x < scaledWidth; x++) {
         const y = lines[lineCount][x];
         const y2 = lines[lineCount + 1]
-          ? lines[lineCount + 1][x]
+          ? lines[lineCount + 1][x] - 10
           : height + 100;
 
         svgLines.push(
